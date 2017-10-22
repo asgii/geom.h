755adbde305b4a08a1ea373e8d6c4e8536814f8c
asgii <32249254+asgii@users.noreply.github.com>
2017-10-22 19:13:43 +0100

    Compiles in cpp

--- geom.h
+++ geom.h
@@ -1,87 +1,103 @@
+#pragma once
+
 #ifdef GEOM_CPP
 #include <stdexcept>
 #endif
 
-typedef geom_vec2 float[2];
-typedef geom_vec3 float[3];
-typedef geom_vec4 float[4];
-typedef geom_mat3 geom_vec3[3];
-typedef geom_mat4 geom_vec4[4];
-typedef geom_quat geom_vec4;
-typedef geom_aa geom_vec4;
+#define GEOM_PI 3.14159265358979323846
+
+typedef struct geom_vec2 { float d[2]; } geom_vec2;
+typedef struct geom_vec3 { float d[3]; } geom_vec3;
+typedef struct geom_vec4 { float d[4]; } geom_vec4;
+typedef struct geom_mat3 { geom_vec3 d[3]; } geom_mat3;
+typedef struct geom_mat4 { geom_vec4 d[4]; } geom_mat4;
+typedef struct geom_quat { float d[4]; } geom_quat;
+typedef struct geom_aa { float d[4]; } geom_aa;
 
 #ifdef GEOM_CPP
-inline
+extern "C" {
 #endif
-float dot(const geom_vec3 p, const geom_vec3 q);
+
+#ifdef GEOM_CPP
+//inline
+#endif
+//float dot(const geom_vec3 p, const geom_vec3 q);
+
+//TODO cpp overloading
+//TODO geom_ prefix on everything? except cpp overloads?
 
 //...
 
-float dot(const geom_vec3 p, const geom_vec3 q)
+float radians(float degr)
 {
-   return p[0] * q[0] +
-      p[1] * q[1] +
-      p[2] * q[2];
+   return degr * 180.f / GEOM_PI;
 }
 
-float dot(const geom_vec2 p, const geom_vec2 q)
+float dot_3x3(const geom_vec3 p, const geom_vec3 q)
 {
-   return p[0] * q[0] +
-      p[1] * q[1];
+   return p.d[0] * q.d[0] +
+      p.d[1] * q.d[1] +
+      p.d[2] * q.d[2];
 }
 
-geom_vec3 cross(const geom_vec3 p, const geom_vec3 q)
+float dot_2x2(const geom_vec2 p, const geom_vec2 q)
 {
-   return {p[1] * q[2] - p[2] * q[1],
-	 p[2] * q[0] - p[0] * q[2],
-	 p[0] * q[1] - p[1] * q[0]};
+   return p.d[0] * q.d[0] +
+      p.d[1] * q.d[1];
 }
 
-geom_vec3 add(const geom_vec3 p, const geom_vec3 q)
+geom_vec3 cross_3x3(const geom_vec3 p, const geom_vec3 q)
 {
-   return {p[0] + q[0],
-	 p[1] + q[1],
-	 p[2] + q[2]};
+   return {p.d[1] * q.d[2] - p.d[2] * q.d[1],
+	 p.d[2] * q.d[0] - p.d[0] * q.d[2],
+	 p.d[0] * q.d[1] - p.d[1] * q.d[0]};
 }
 
-geom_vec3 sub(const geom_vec3 p, const geom_vec3 q)
+geom_vec3 add_3x3(const geom_vec3 p, const geom_vec3 q)
 {
-   return {p[0] - q[0],
-	 p[1] - q[1],
-	 p[2] - q[2]};
+   return {p.d[0] + q.d[0],
+	 p.d[1] + q.d[1],
+	 p.d[2] + q.d[2]};
 }
 
-geom_vec3 mul(const geom_vec3 p, const float scalar)
+geom_vec3 sub_3x3(const geom_vec3 p, const geom_vec3 q)
 {
-   return {p[0] * scalar,
-	 p[1] * scalar,
-	 p[2] * scalar};
+   return {p.d[0] - q.d[0],
+	 p.d[1] - q.d[1],
+	 p.d[2] - q.d[2]};
 }
 
-geom_vec3 div(const geom_vec3 p, const float divisor)
+geom_vec3 mul_3x1(const geom_vec3 p, const float scalar)
 {
-   return {p[0] / divisor,
-	 p[1] / divisor,
-	 p[2] / divisor};
+   return {p.d[0] * scalar,
+	 p.d[1] * scalar,
+	 p.d[2] * scalar};
 }
 
-geom_vec2 div(const geom_vec2 p, const float divisor)
+geom_vec3 div_3x1(const geom_vec3 p, const float divisor)
 {
-   return {p[0] / divisor,
-	 p[1] / divisor};
+   return {p.d[0] / divisor,
+	 p.d[1] / divisor,
+	 p.d[2] / divisor};
+}
+
+geom_vec2 div_2x1(const geom_vec2 p, const float divisor)
+{
+   return {p.d[0] / divisor,
+	 p.d[1] / divisor};
 }
 	 
-float len(const geom_vec2 p)
+float len_2(const geom_vec2 p)
 {
-   return sqrtf(dot(p, p));
+   return sqrtf(dot_2x2(p, p));
 }
 
-float len(const geom_vec3 p)
+float len_3(const geom_vec3 p)
 {
-   return sqrtf(dot(p, p));
+   return sqrtf(dot_3x3(p, p));
 }
 
-geom_vec3 norm(const geom_vec3 p)
+geom_vec3 norm_3(const geom_vec3 p)
 {
    /*
      A unit vector has a length of 1.
@@ -90,45 +106,45 @@ geom_vec3 norm(const geom_vec3 p)
      same way that dividing a scalar by its own magnitude will return 1.
    */
 
-   return div(p, len(p));
+   return div_3x1(p, len_3(p));
 }
 
-geom_vec3 inverse(const geom_vec3 p)
+geom_vec3 inverse_3(const geom_vec3 p)
 {
-   return {-p[0],
-	 -p[1],
-	 -p[2]};
+   return {-p.d[0],
+	 -p.d[1],
+	 -p.d[2]};
 }
 
-float len_quat(const geom_quat p)
+float len_q(const geom_quat p)
 {
-   return sqrtf(p[3] * p[3] +
-		p[0] * p[0] +
-		p[1] * p[1] +
-		p[2] * p[2]);
+   return sqrtf(p.d[3] * p.d[3] +
+		p.d[0] * p.d[0] +
+		p.d[1] * p.d[1] +
+		p.d[2] * p.d[2]);
 }
 
-geom_quat norm_quat(const geom_quat p)
+geom_quat norm_q(const geom_quat p)
 {
-   float len = len_quat(p);
+   float len = len_q(p);
 
-   return {p[0] / len,
-	 p[1] / len,
-	 p[2] / len,
-	 p[3] / len};
+   return {p.d[0] / len,
+	 p.d[1] / len,
+	 p.d[2] / len,
+	 p.d[3] / len};
 }
 
-bool is_unit_quat(const geom_quat p, float epsilon)
+bool is_unit_q(const geom_quat p, float epsilon)
 {
    //A quaternion is normalised iff it has a length of 1.
    //Its length is equal to sqrt(...).
    //sqrt(1) = 1.
    //So just check that ... == 1.
 
-   float sum = p[3] * p[3] +
-      p[0] * p[0] +
-      p[1] * p[1] +
-      p[2] * p[2];
+   float sum = p.d[3] * p.d[3] +
+      p.d[0] * p.d[0] +
+      p.d[1] * p.d[1] +
+      p.d[2] * p.d[2];
 
    if ((sum > (1 + epsilon)) || (sum < (1 - epsilon)))
    {
@@ -138,22 +154,22 @@ bool is_unit_quat(const geom_quat p, float epsilon)
    else { return true; }
 }
 
-geom_quat conjugate_quat(const geom_quat p)
+geom_quat conjugate_q(const geom_quat p)
 {
-   const geom_vec3& vec = p;
+   geom_vec3 vec = {.d = {p.d[0], p.d[1], p.d[2]}};
 
-   geom_vec3 inv_vec = inverse(vec);
+   geom_vec3 inv_vec = inverse_3(vec);
 
-   return {inv_vec[0], inv_vec[1], inv_vec[2], p[3]};
+   return {inv_vec.d[0], inv_vec.d[1], inv_vec.d[2], p.d[3]};
 }
 
-geom_quat inverse_quat(const geom_quat p)
+geom_quat inverse_q(const geom_quat p)
 {
    const float epsilon = 0.01;
 
-   geom_quat conj = conjugate_quat(p);
+   geom_quat conj = conjugate_q(p);
 
-   if (is_unit_quat(p, epsilon))
+   if (is_unit_q(p, epsilon))
    {
       return conj;
    }
@@ -161,116 +177,133 @@ geom_quat inverse_quat(const geom_quat p)
    //Else divide conjugate's elements by sum of non-conjugate squares.
    else
    {
-      float sum_sq = p[0] * p[0] +
-	 p[1] * p[1] +
-	 p[2] * p[2] +
-	 p[3] * p[3];
+      float sum_sq = p.d[0] * p.d[0] +
+	 p.d[1] * p.d[1] +
+	 p.d[2] * p.d[2] +
+	 p.d[3] * p.d[3];
 
-      return {conj[0] / sum_sq,
-	    conj[1] / sum_sq,
-	    conj[2] / sum_sq,
-	    conj[3] / sum_sq};
+      return {conj.d[0] / sum_sq,
+	    conj.d[1] / sum_sq,
+	    conj.d[2] / sum_sq,
+	    conj.d[3] / sum_sq};
    }
 }
 
 geom_quat quat(const geom_aa p)
 {
-   const float& angle = p[3];
-   const geom_vec3& axis = p;
+   float angle = p.d[3];
+   geom_vec3 axis = {.d = {p.d[0], p.d[1], p.d[2]}};
 
    float sin_a = sin(radians(angle / 2.f));
    float cos_a = cos(radians(angle / 2.f));
 
-   geom_vec3 vec = axis * sin_a;
+   geom_vec3 vec = mul_3x1(axis, sin_a);
    
-   geom_quat quat = {vec[0], vec[1], vec[2], cos_a};
+   geom_quat quat = {vec.d[0], vec.d[1], vec.d[2], cos_a};
 
    const float epsilon = 0.01;
    
-   if (!is_unit_quat(quat, epsilon))
+   if (!is_unit_q(quat, epsilon))
    {
-      quat = norm_quat(quat);
+      quat = norm_q(quat);
    }
 
    return quat;
 }
 
-geom_quat mul_quat(const geom_quat p, const geom_quat q)
+geom_quat mul_qxq(const geom_quat p, const geom_quat q)
 {
    //If either involves an angle of 0, it doesn't involve any rotation.
 
-   if (p[3] == 0.f) { return q; }
+   if (p.d[3] == 0.f) { return q; }
 
-   else if (q[3] == 0.f) { return p; }
+   else if (q.d[3] == 0.f) { return p; }
 
-   const geom_vec3& pvec = p;
-   const geom_vec3& qvec = q;
+   const geom_vec3 pvec = {.d = {p.d[0], p.d[1], p.d[2]}};
+   const geom_vec3 qvec = {.d = {q.d[0], q.d[1], q.d[2]}};
 
-   geom_vec3 vec = cross(pvec, qvec) +
-      mul(pvec, q[3]) +
-      mul(qvec, p[3]);
+   geom_vec3 vec = add_3x3(cross_3x3(pvec, qvec),
+			   add_3x3(mul_3x1(pvec, q.d[3]),
+				   mul_3x1(qvec, p.d[3])));
    
-   float w = p[3] * q[3] - dot(p, q);
+   float w = p.d[3] * q.d[3] - dot_3x3(pvec, qvec);
 
    //NB unit quaternions' product should itself be a unit quaternion.
-   return {vec[0], vec[1], vec[2], w};
+   //I don't think there's any advantage in checking, though (how
+   //would you know whether the inputs were meant to be unit-length?)
+   //Rather, the user should check the inputs beforehand.
+   return {vec.d[0], vec.d[1], vec.d[2], w};
 }
 
-geom_vec3 rotate(const geom_quat p, const geom_vec3 q)
+geom_vec3 rotate_qxq(const geom_quat p, const geom_vec3 q)
 {
    //p * q * conjugate(p)
-   //conjugate(p) = -p[0], -p[1], -p[2], p[3]
+   //conjugate(p) = -p.d[0], -p.d[1], -p.d[2], p.d[3]
 
    //For convenience in writing the below mul.
-   geom_vec3 psq = {p[0] * p[0], p[1] * p[1], p[2] * p[2]};
+   geom_vec3 psq = {p.d[0] * p.d[0], p.d[1] * p.d[1], p.d[2] * p.d[2]};
 
    //This is just a simplified subset of a 3x3 matrix-vector mul.
 
    geom_vec3 res;
 
-   res[0] = (q[0] * (0.5 - psq[1] - psq[2]) +
-	     q[1] * (p[0] * p[1] - p[3] * p[2]) +
-	     q[2] * (p[0] * p[2] + p[1] * p[2]));
+   res.d[0] = (q.d[0] * (0.5 - psq.d[1] - psq.d[2]) +
+	       q.d[1] * (p.d[0] * p.d[1] - p.d[3] * p.d[2]) +
+	       q.d[2] * (p.d[0] * p.d[2] + p.d[1] * p.d[2]));
 
-   res[1] = (q[0] * (p[0] * p[1] + p[2] * p[3]) +
-	     q[1] * (0.5 - psq[0] - psq[2]) + 
-	     q[2] * (p[1] * p[2] - p[0] * p[3]));
+   res.d[1] = (q.d[0] * (p.d[0] * p.d[1] + p.d[2] * p.d[3]) +
+	       q.d[1] * (0.5 - psq.d[0] - psq.d[2]) + 
+	       q.d[2] * (p.d[1] * p.d[2] - p.d[0] * p.d[3]));
 
-   res[2] = (q[0] * (p[0] * p[2] - p[1] * p[3]) +
-	     q[1] * (p[1] * p[2] + p[0] * p[4]) +
-	     q[2] * (0.5 - psq[0] - psq[1]));
+   res.d[2] = (q.d[0] * (p.d[0] * p.d[2] - p.d[1] * p.d[3]) +
+	       q.d[1] * (p.d[1] * p.d[2] + p.d[0] * p.d[3]) +
+	       q.d[2] * (0.5 - psq.d[0] - psq.d[1]));
 
-   res = mul(res, 2.f);
+   res = mul_3x1(res, 2.f);
 
    return res;
 }
 
 #ifdef GEOM_CPP
+} //extern "C"
+#endif
+
+#ifdef GEOM_CPP
 namespace geom
 {
-   class vec3
+class vec3
+{
+private:
+   geom_vec3 vec;
+
+   vec3 (geom_vec3 xyz) : vec (xyz) {}
+      
+public:
+
+   vec3 (float x, float y, float z) : vec ({.d = {x, y, z}}) {}
+
+   const float& operator[] (const size_t ind) const
    {
-     private:
-      geom_vec3 vec;
+      return vec.d[ind];
+   }
+
+   float& operator[] (const size_t ind)
+   {
+      return vec.d[ind];
+   }
       
-     public:
-     vec3(float x, float y, float z) : vec ({.x = x, .y = y, .z = z}) {}
+   vec3 operator+ (const vec3 q) const { return vec3(add_3x3(vec, q.vec)); }
+   vec3 operator- (const vec3 q) const { return vec3(sub_3x3(vec, q.vec)); }
+   vec3 operator* (const float scalar) const { return vec3(mul_3x1(vec, scalar)); }
+   vec3 operator/ (const float scalar) const { return vec3(div_3x1(vec, scalar)); }
 
-      const float& operator[] (const size_t ind) const
-      {
-	 return vec[ind];
-      }
-      
-      vec3 operator+ (const vec3 q) const { return add(vec, q.vec); }
-      vec3 operator- (const vec3 q) const { return sub(vec, q.vec); }
-      vec3 operator* (const float scalar) const { return mul(vec, scalar); }
-      vec3 operator/ (const float scalar) const { return mul(vec, scalar); }
+   float len() const { return len_3(vec); }
+   vec3 norm() const { return vec3(norm_3(vec)); }
+   vec3 inverse() const { return vec3(inverse_3(vec)); }
 
-      float getLength() const { return len(vec); }
-      vec3 getNormal() const { return norm(vec); }
-      vec3 getInverse() const { return inverse(vec); }
-   };
+   friend float dot(const vec3 p, const vec3 q) { return dot_3x3(p.vec, q.vec); }
+   friend vec3 cross(const vec3 p, const vec3 q) { return vec3(cross_3x3(p.vec, q.vec)); }
 
-   vec3 dot(const vec3 p, const vec3 q) { return dot(p.vec, q.vec); }
+};
 }
 #endif
